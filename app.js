9d5299ca13d4685e3e02eb5e21fea74a7db27d59
zendi014 <48636084+zendi014@users.noreply.github.com>
2019-10-09 23:45:05 +0700

    add api
    add api

--- app.js
+++ app.js
@@ -7,20 +7,40 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+
+
+
 var app = express();
 
+
+
+
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+
+app.use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use(cookieParser())
+  .use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+app.use('/', indexRouter) // content routers
+  .use('/users', usersRouter)
+  .use('/api', require('./api/index'));
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
