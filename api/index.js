30dc5e2ec5754263fe78b660441165011b0c9c60
zendi014 <48636084+zendi014@users.noreply.github.com>
2019-10-19 13:54:23 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,11 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+// localhost:3000/api/sensors
+router.get('/sensors', (req, res)=>{
+  dt = {
+    "service_name" : "get_all_sensor"
+  }
+  res.json(dt)
+})
 
-router.get('/', function(req, res, next) {
-  let dt = { title: 'Express' }
-  res.json(dt);
-});
+
+// localhost:3000/api/sensors/dht-11 => has id
+router.get('/sensors/:id', (req, res)=>{
+  dt = {
+    "service_name" : "get_sensor_by_id",
+    "id" : req.params.id, // depend on params 
+  }
+  res.json(dt)
+})
+
+router.post("/sensors", (req, res)=>{
+  console.log(req.body)
+  dt = {
+    "data" : req.body,
+    "service_name" : "get_all_sensor_by_id",
+  }
+  res.json(dt)
+})
+
+
+
+
 
 
 module.exports = router;
