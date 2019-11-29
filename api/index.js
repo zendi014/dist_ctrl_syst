08bbb6836fcd2176adbba5bc2152af61ed2bd723
zendi014 <48636084+zendi014@users.noreply.github.com>
2019-11-29 17:02:55 +0700

    update index

--- api/index.js
+++ api/index.js
@@ -7,26 +7,38 @@ let fs = require("./sdk/connection")
 let sensors = fs.collection("sensors")
 
 
-let source_key = md5("k1");
-let destination_key = md5("k5");
+// let source_key = "G1"//md5("k1");
+// let destination_key = "G2"//md5("k5");
+
 
 router.get('/sensor_transactions', (req, res)=>{
-    let dt = []
-    sensors.get().then((snap)=>{
-        snap.forEach((s)=>{
-            dt.push(s.data())
-        })
-        res.json(dt);
+    sensors.doc("3A1nfVFeReL3LtluC7yP").get().then((snap)=>{
+        res.json(snap.data());
     })
 })
 
 
+
+router.post('/sensor_transaction', (req, res)=>{
+    let data = {
+        "source_key": req.body.source_key,
+        "value": req.body.value,
+        "destination_key": req.body.destination_key,
+    }
+    sensors.doc("3A1nfVFeReL3LtluC7yP").update(data).then((snap)=>{
+        res.json(data)
+    })  
+})
+
+
+/*
+//case when created
 router.post('/sensor_transaction', (req, res)=>{
     let data = {
         "transaction_key": "",
         "source_key": source_key,
         "value": req.body.value,
-        "destination_key": md5(req.body.destination_key),
+        "destination_key": req.body.destination_key,
     }
     sensors.add(data).then((snap)=>{
         snap.update({
@@ -38,7 +50,28 @@ router.post('/sensor_transaction', (req, res)=>{
 })
 
 
+//get all data
+router.get('/sensor_transactions', (req, res)=>{
+    let dt = []
+    sensors.get().then((snap)=>{
+        snap.forEach((s)=>{
+            dt.push(s.data())
+        })
+        res.json(dt);
+    })
+})
 
+
+
+//get total data
+router.get('/total_sensor_transactions', (req, res)=>{
+    sensors.get().then((snap)=>{
+        res.json(snap.size);
+    });
+})
+
+
+//get data with key
 router.get('/sensor_transaction/:transaction_key', (req, res)=>{ // name, id, key
   sensors.doc(req.params.transaction_key)
       .get().then((snap)=>{
@@ -48,7 +81,7 @@ router.get('/sensor_transaction/:transaction_key', (req, res)=>{ // name, id, ke
 
 
 
-
+//case when update
 router.put('/sensor_transaction/:transaction_key', (req, res)=>{
     let data = {
         "transaction_key": req.params.transaction_key,
@@ -62,6 +95,23 @@ router.put('/sensor_transaction/:transaction_key', (req, res)=>{
         }        
     })  
 });
+
+
+
+
+//delete data
+router.get('/clean_sensor_transaction', (req, res)=>{ // name, id, key
+    sensors.get().then((snap)=>{
+        snap.forEach((s)=>{
+            sensors.doc(s.id).delete()
+        })
+        res.json("collection deleted...");
+    }) 
+});
+  
+
+*/
+
 
 
 module.exports = router;
